ae429dd8fd26751ffb7701860f35d74ffc1d6538
oHminod <yvesm13@gmail.com>
2024-10-31 14:30:57 +0100

    try catch block

--- src/utils/data-access-layer.js
+++ src/utils/data-access-layer.js
@@ -1,38 +1,47 @@
 export const signinUser = async (email, password) => {
-  const response = await fetch("http://localhost:3001/api/v1/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-  if (!response.ok) return response;
+    if (!response.ok) return response;
 
-  const data = await response.json();
-  const token = data.body.token;
+    const data = await response.json();
+    const token = data.body.token;
 
-  return { ok: response.ok, token };
+    return { ok: response.ok, token };
+  } catch (error) {
+    console.error("signinUser error:", error);
+    return error;
+  }
 };
 
 export const getUser = async (token) => {
-  const response = await fetch("http://localhost:3001/api/v1/user/profile", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
+  try {
+    const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-  if (!response.ok) return response;
+    if (!response.ok) return response;
 
-  const data = await response.json();
-  //   console.log(data.body);
+    const data = await response.json();
 
-  const userData = data.body;
+    const userData = data.body;
 
-  return { ok: response.ok, userData };
+    return { ok: response.ok, userData };
+  } catch (error) {
+    console.error("getUser error:", error);
+    return error;
+  }
 };
