69794b856ba262e01a71c7e269ffaa53afbae189
oHminod <yvesm13@gmail.com>
2024-11-06 22:25:10 +0100

    internal error status

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = ({ error }) => {
   if (typeof error === "string") {
     return (
       <main className="center-wrapper">
-        <h1>520</h1>
+        <h1>500</h1>
         <p className="error-message">{error}</p>
         <Link to="/">Back to Home</Link>
       </main>
